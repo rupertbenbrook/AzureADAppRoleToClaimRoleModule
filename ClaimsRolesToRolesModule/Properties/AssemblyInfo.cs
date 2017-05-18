6054088191f9ab942605222c205d601e08645dee
Rupert Benbrook <rupert@benbrook.org.uk>
2017-05-18 20:57:14 +0100

    Starting to add the basics of NuGet package.

--- ClaimsRolesToRolesModule/Properties/AssemblyInfo.cs
+++ ClaimsRolesToRolesModule/Properties/AssemblyInfo.cs
@@ -1,34 +1,16 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("ClaimsRolesToRolesModule")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Copies Azure Active Directory application role claims to ClaimsIdentity role claims.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyCompany("Rupert Benbrook")]
 [assembly: AssemblyProduct("ClaimsRolesToRolesModule")]
-[assembly: AssemblyCopyright("Copyright © Microsoft Corporation 2017")]
+[assembly: AssemblyCopyright("Copyright © Rupert Benbrook 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b43eb084-d220-4dfe-939b-f0096bc193e3")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
