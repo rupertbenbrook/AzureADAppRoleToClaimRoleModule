bdb551cfa265eaa3308d24972a7b8d0b6a435c9e
Rupert Benbrook <rupert@benbrook.org.uk>
2017-05-18 16:35:57 +0100

    Add project files.

---
+++ ClaimsRolesToRolesModule/ClaimsRolesToRolesModule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Claims;
+using System.Web;
+
+namespace ClaimsRolesToRolesModule
+{
+    // https://blogs.msdn.microsoft.com/waws/2017/03/09/azure-app-service-authentication-app-roles/
+
+    public class ClaimsRolesToRolesModule : IHttpModule
+    {
+
+        public void Init(HttpApplication context)
+        {
+            ModuleTrace.ModuleInit();
+            context.PostAuthenticateRequest += OnPostAuthenticateRequest;
+        }
+
+        public void Dispose()
+        {
+            ModuleTrace.ModuleDispose();
+        }
+
+        private void OnPostAuthenticateRequest(object sender, EventArgs e)
+        {
+            ModuleTrace.ModulePostAuthenticate();
+            if (ClaimsPrincipal.Current.Identity.IsAuthenticated)
+            {
+                ClaimsIdentity claimsIdentity = ClaimsPrincipal.Current.Identity as ClaimsIdentity;
+                foreach (var claim in ClaimsPrincipal.Current.FindAll("roles"))
+                {
+                    var roleClaim = new Claim(claimsIdentity.RoleClaimType, claim.Value);
+                    claimsIdentity.AddClaim(claim);
+                    ModuleTrace.ClaimCopy(claim, roleClaim);
+                }
+            }
+        }
+    }
+}
---
+++ ClaimsRolesToRolesModule/ModuleTrace.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Security.Claims;
+
+namespace ClaimsRolesToRolesModule
+{
+    public static class ModuleTrace
+    {
+        public static void ModuleInit()
+        {
+            Trace.TraceInformation("ClaimsRolesToRolesModule: Init");
+        }
+
+        public static void ModuleDispose()
+        {
+            Trace.TraceInformation("ClaimsRolesToRolesModule: Dispose");
+        }
+
+        public static void ModulePostAuthenticate()
+        {
+            Trace.TraceInformation("ClaimsRolesToRolesModule: Post Authenticate");
+        }
+
+        public static void ClaimCopy(Claim source, Claim destination)
+        {
+            Trace.TraceInformation("ClaimsRolesToRolesModule: Copied claim {0} to claim {1}", source, destination);
+        }
+    }
+}
